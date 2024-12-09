7870c22a483fdabd6a6fa4b0c39dc3042d2d1179
rahul verma <48601181+rahulverma9449@users.noreply.github.com>
2024-12-09 01:56:33 +0000

    comit

---
+++ nextjs-project/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const HeroSection = () =>{
+    return (
+    <section> 
+        <h1 className="text-white">Portfolio Website by Rahul Verma</h1>
+        <p className="text-[#ADB7BE]">
+            I am rahul verma. I am working with HCLTech as a Lead Engineer.
+        </p>
+    </section>
+    );
+}
+
+export default HeroSection;
---
+++ nextjs-project/app/page.jsx
@@ -0,0 +1,11 @@
+
+import HeroSection from "./components/HeroSection"; // Adjust extension if necessary
+
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col bg-[#210101] container mx-auto px-12 py-4">
+      <HeroSection />
+      </main>
+     
+  );
+}
